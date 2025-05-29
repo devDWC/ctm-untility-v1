9cf424ef5091afd3fe038cdc9953b38ceea420d5
NhatVoDev <132722718+NhatVoDev@users.noreply.github.com>
2025-05-29 21:43:44 +0700

    ádsad

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -31,3 +31,17 @@ export const s3Untility: {
     organization: string
   ) => Promise<any>;
 };
+// ctm-library/index.d.ts
+export const mgoUtility: {
+  buildMongoQuery: (params: {
+    search?: string;
+    searchKeys?: string[];
+    sortList?: { [key: string]: 1 | -1 }[];
+    defaultSort?: { [key: string]: 1 | -1 };
+    baseFilter?: Record<string, any>;
+  }) => {
+    filter: Record<string, any>;
+    sort: Record<string, 1 | -1>;
+  };
+};
+
